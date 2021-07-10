d42e17730913509b9fab92fc0e56ee7c90751fbc
Jose Ignacio Huby Ochoa <40769942+nutax@users.noreply.github.com>
2021-07-10 01:08:10 -0500

    CrossOrigin

--- src/main/java/com/academic/projection/controller/ViewerController.java
+++ src/main/java/com/academic/projection/controller/ViewerController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "/viewers")
+@CrossOrigin
 public class ViewerController {
     @Autowired
     private PredictionService predictionService;

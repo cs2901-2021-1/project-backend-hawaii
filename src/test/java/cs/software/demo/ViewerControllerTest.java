a3191fe6b22843e306ebb7614bae7357bbf6772b
cecharre <claudio.echarre@utec.edu.pe>
2021-07-12 15:35:12 -0500

    add test

---
+++ src/test/java/cs/software/demo/ViewerControllerTest.java
@@ -0,0 +1,27 @@
+package cs.software.demo;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+public class ViewerControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getAllPrediction() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/viewers")).andExpect(status().isOk());
+    }
+}

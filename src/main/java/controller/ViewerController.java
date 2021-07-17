3205b5c969318264e33c9e78dbc3a34baac409d8
Jose Ignacio Huby Ochoa <40769942+nutax@users.noreply.github.com>
2021-07-17 17:43:02 -0500

    Update ViewerController.java

--- src/main/java/controller/ViewerController.java
+++ src/main/java/controller/ViewerController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.logging.Logger;
-
+import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController

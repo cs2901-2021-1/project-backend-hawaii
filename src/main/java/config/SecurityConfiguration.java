fb0a125706051f0fb116f0f554dafd02482b16e4
Jose Ignacio Huby Ochoa <40769942+nutax@users.noreply.github.com>
2021-07-17 18:02:43 -0500

    Update SecurityConfiguration.java

--- src/main/java/config/SecurityConfiguration.java
+++ src/main/java/config/SecurityConfiguration.java
@@ -1,12 +1,10 @@
 package config;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableOAuth2Sso
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
@@ -15,7 +13,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .httpBasic().disable()
             .antMatcher("/**").authorizeRequests()
             .antMatchers("/sessions/github/callback").authenticated()
-            .anyRequest().permitAll();
+            .anyRequest().permitAll()
+            .and()
+            .oauth2Login();
     }
     
 }

1cc3ecac6a0a3fc90bc3cc4afe13958703fb414a
Jose Ignacio Huby Ochoa <40769942+nutax@users.noreply.github.com>
2021-07-18 17:16:09 -0500

    Update TIController.java

--- src/main/java/controller/TIController.java
+++ src/main/java/controller/TIController.java
@@ -7,6 +7,7 @@ import data.dtos.ViewerDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 import java.sql.SQLException;
 import java.io.IOException;

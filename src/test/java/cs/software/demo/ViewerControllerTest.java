bff9aaa0887718b4e4206347dc96be669309f1a6
Jose Ignacio Huby Ochoa <40769942+nutax@users.noreply.github.com>
2021-07-19 11:53:08 -0500

    Update ViewerControllerTest.java

--- src/test/java/cs/software/demo/ViewerControllerTest.java
+++ src/test/java/cs/software/demo/ViewerControllerTest.java
@@ -39,6 +39,6 @@ public class ViewerControllerTest {
 
     @Test
     public void getAllPrediction() throws Exception {
-        mvc.perform(get("/viewers").with(authentication(getOauthAuthenticationFor(principal))))).andExpect(status().isFound());
+        mvc.perform(get("/viewers").with(authentication(getOauthAuthenticationFor(principal)))).andExpect(status().isFound());
     }
 }

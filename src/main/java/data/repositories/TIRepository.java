ef0951f705d1fd875ad1a5dc1ea7e803e3b57500
cecharre <claudio.echarre@utec.edu.pe>
2021-07-23 00:41:29 -0500

    changes Viewer and TI for User

--- src/main/java/data/repositories/TIRepository.java
+++
@@ -1,8 +0,0 @@
-package data.repositories;
-
-import data.entities.TI;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TIRepository extends JpaRepository<TI, String> {
-
-}
--- src/main/java/data/repositories/ViewerRepository.java
+++
@@ -1,8 +0,0 @@
-package data.repositories;
-
-import data.entities.Viewer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ViewerRepository extends JpaRepository<Viewer, String> {
-
-}

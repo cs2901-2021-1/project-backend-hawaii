1db47d50325c5cafca62088dd42a041989c5d1f3
cecharre <claudio.echarre@utec.edu.pe>
2021-07-23 01:35:33 -0500

    fix authorize

--- src/main/java/business/AuthorizationService.java
+++ src/main/java/business/AuthorizationService.java
@@ -46,9 +46,13 @@ public class AuthorizationService{
     }
 
     public void authorize(OAuth2User user, char type) throws UnauthorizedException{
-        if(user == null || userRepository.findByEmailAndType((String)user.getAttributes().get("email"),type).isEmpty()){
+        if(user == null || userRepository.findById((String)user.getAttributes().get("email")).isEmpty()){
             throw new UnauthorizedException();
         }
+        if(userRepository.findByEmailAndType((String) user.getAttributes().get("email"), type).isEmpty()&&TYPE_ADMIN==type){
+            throw new UnauthorizedException();
+        }
+
     }
 
     public List<UserDTO> getViewers(){
